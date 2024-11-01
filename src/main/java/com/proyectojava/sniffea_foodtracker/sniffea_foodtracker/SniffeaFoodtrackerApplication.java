5efe5f3b5bd16622d5b480412eb6c8365847917f
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-11-01 19:40:05 -0300

    commit incial
    commit inicial proyecto

---
+++ src/main/java/com/proyectojava/sniffea_foodtracker/sniffea_foodtracker/SniffeaFoodtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.proyectojava.sniffea_foodtracker.sniffea_foodtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SniffeaFoodtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SniffeaFoodtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectojava/sniffea_foodtracker/sniffea_foodtracker/SniffeaFoodtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectojava.sniffea_foodtracker.sniffea_foodtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SniffeaFoodtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
