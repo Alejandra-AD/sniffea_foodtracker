ec1395ce1f7bca337e284963ddaebd6d29f2ef2a
Alejandra C. Araya <Arayadiaz.ac@gmail.com>
2024-11-15 12:00:17 -0300

    Update Food.java

--- src/main/java/com/proyectojava/sniffea_foodtracker/sniffea_foodtracker/models/entities/Food.java
+++ src/main/java/com/proyectojava/sniffea_foodtracker/sniffea_foodtracker/models/entities/Food.java
@@ -5,16 +5,17 @@ import java.util.List;
 import com.proyectojava.sniffea_foodtracker.sniffea_foodtracker.models.dtos.CommentDTO;
 import com.proyectojava.sniffea_foodtracker.sniffea_foodtracker.models.dtos.LocationDTO;
 import com.proyectojava.sniffea_foodtracker.sniffea_foodtracker.models.dtos.UserDTO;
+import com.proyectojava.sniffea_foodtracker.sniffea_foodtracker.models.enums.PetType;
 
 public class Food {
 
     private Long id;
     private String brand;
-    private String typePet; //evaluar si será un enum*
+    private PetType petType; //evaluar si será un enum*
     private Double price;
     private LocationDTO location;
     private UserDTO user;
-    private List<CommentDTO>comment;//evaluar si se necesita una clase list generica
+    private List<CommentDTO>comment;
 
 }
 
